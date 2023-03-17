f5439d35b4012b7a953623bab087a2d6a1a63d2d
dilipsuthar1999 <61174714+dilipsuthar1999@users.noreply.github.com>
2023-03-17 12:11:29 -0700

    fixing css

--- src/components/ContentSection.js
+++ src/components/ContentSection.js
@@ -6,7 +6,7 @@ const ContentSection = ({ section, data, url }) => {
     return (
         <section className={section === "update" ? "bg-color overflow-hidden" : null}>
             <div className="container py-5">
-                <div className="row align-items-center">
+                <div className="row align-items-center py-4">
                     <div className={`col-md-6 text-center my-3 ${section === "update" && "order-2"}`}>
                         <img src={data.img} className={section === "update" ? "update-img pl-4" : "w-75"} />
                     </div>
